58e33cd78143077253d5cac86578b86f3d99ddc4
William Onsare <william.onsare254@gmail.com>
2023-08-07 17:38:44 +0300

    add widget on sample site

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './Nairobi_City_Logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>NAIROBI CITY COUNTY</h1>
+        <p>Lets Make Nairobi Work</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
